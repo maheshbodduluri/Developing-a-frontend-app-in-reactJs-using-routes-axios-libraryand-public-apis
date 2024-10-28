0a8e7b542cc974c07c8f351b135a5b8137f7bfb8
maheshbodduluri <144084407+maheshbodduluri@users.noreply.github.com>
2024-10-28 11:45:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
+import Users from './Users';
 import './App.css';
-
+import axios from 'axios';
+import {useState,useEffect} from 'react';
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.map((user) => {
+        return (
+          <Users
+            key={user.id}
+            name={user.name}
+            username={user.username}
+            email={user.email}
+            phone={user.phone}
+            website={user.website}
+          />
+        )
+      })}
     </div>
   );
 }
